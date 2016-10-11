57b031a509fa734f596fb545cdc24f8ec016799d
JinKim95 <jin0410@vt.edu>
2016-10-11 11:27:42 -0400

    FINAL COMMIT

---
+++ out/webhelp-responsive/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,7 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "Main Introduction Task.html";
+fl["1"]= "User Doc Task 1.html";
+fl["2"]= "User Doc Task 2.html";
+fl["3"]= "User Doc Task 3.html";
+var doStem = false;searchLoaded = true;
